1cb4ac953c01b1db8ddf2445de1dacd2a8699b91
Griffin Cosgrove <griffcosgrove@gmail.com>
2019-07-17 09:24:48 -0400

    7/17/19 added forecast, next add logic to email forecast to me.

--- weather_bot.py
+++ weather_bot.py
@@ -1,3 +1,26 @@
-#purpose of this program is to create a bot that texts me with the weather for each day.
-#having trouble coding on desktop since windows got corrupted and lost several files
-print("Hello World")
+# purpose of this program is to create a bot that texts me with the weather for each day.
+
+from darksky.api import DarkSky, DarkSkyAsync
+from darksky.types import languages, units, weather
+
+API_KEY = '984f861bb9c84baa5744eb667835b824'
+
+darksky = DarkSky(API_KEY)
+
+latitude = 38.998440
+
+longitude = -77.293022
+
+# cords for Harrisonburg
+# 38.435982
+# -78.879997
+
+forecast = darksky.get_forecast(
+    latitude, longitude,
+    extend=False, # default `False`
+    lang=languages.ENGLISH, # default `ENGLISH`
+    units=units.AUTO, # default `auto`
+    exclude=[weather.MINUTELY, weather.ALERTS] # default `[]`
+)
+
+# add logic to send to email.
