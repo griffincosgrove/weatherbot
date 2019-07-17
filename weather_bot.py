0d34c767bff7613389760c33ab2fab73ef12389f
Griffin Cosgrove <griffcosgrove@gmail.com>
2019-07-16 22:40:20 -0400

    Added comments to python file

---
+++ weather_bot.py
@@ -0,0 +1,3 @@
+#purpose of this program is to create a bot that texts me with the weather for each day.
+#having trouble coding on desktop since windows got corrupted and lost several files
+print("Hello World")
