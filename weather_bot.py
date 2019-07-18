595a5b1b6ba4b42112e8c46f09da78e3701c21c4
Griffin Cosgrove <griffcosgrove@gmail.com>
2019-07-18 15:11:46 -0400

    Added logic to currently return the daily forecast summary #1

--- weather_bot.py
+++ weather_bot.py
@@ -2,6 +2,7 @@
 
 from darksky.api import DarkSky, DarkSkyAsync
 from darksky.types import languages, units, weather
+import datetime
 
 API_KEY = '984f861bb9c84baa5744eb667835b824'
 
@@ -25,13 +26,26 @@ forecast = darksky.get_forecast(
 
 forecast.latitude
 forecast.longitude
-forecast.timezone # timezone for coordinates. For exmaple: `America/New_York`
-
-print('today: ' + forecast.currently.summary)
-print(forecast.hourly.summary)
-print('it is: ' + str(forecast.currently.temperature) + ' degrees')
-print('it feels like: ' + str(forecast.currently.apparent_temperature) + ' degrees')
+forecast.timezone
 
 
-# figure out how to print forecast.daily.data
+def daily_forecast():
+    daily_summary = ''
+    for item in forecast.daily.data:
+        daily_summary = ('High is ' + str(item.temperature_high) + " but it will feel like " +
+                         (str(item.apparent_temperature_high)))
+        daily_summary += (' at ' + str(item.temperature_high_time.strftime("%I %p")))
+        daily_summary += '\n'
+        daily_summary += ('Low is ' + str(item.temperature_low) +  " but it will feel like " +
+                          (str(item.apparent_temperature_low)))
+        daily_summary += ' at ' + str(item.temperature_low_time.strftime("%I %p"))
+        daily_summary += '\n'
+        daily_summary += 'With a ' + str(item.precip_probability) + ' chance of precipitation'
+        break
+
+    return daily_summary
+
+
+daily_forecast()
+
 # add logic to send to email.
